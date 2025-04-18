b9fac82cbb51c6748ce8558a5c6e75135fe88f67
thelastandrew <thelastandrew@gmail.com>
2025-04-18 17:23:57 +0300

    tech: add console log

--- src/pl/routers/botRouter.ts
+++ src/pl/routers/botRouter.ts
@@ -10,6 +10,7 @@ export const router = Router();
 router.use('/', rawTextMiddleware);
 router.post('/', async (req: Request, res: Response) => {
   const body = await receiveTelegramMessageHandler(req);
+  logging.info(body);
   await sendMessageHandler(body.chatId, 'hello world');
 
   res.status(200).send('Request handled');
