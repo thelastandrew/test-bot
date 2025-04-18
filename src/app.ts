5647e9272bf3dd4f6cf2b73afe22a68f7ed6dca4
thelastandrew <thelastandrew@gmail.com>
2025-04-18 17:18:55 +0300

    feat: add logging

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express, { json } from 'express';
 import { botRouter } from '@/pl';
+import { loggingHandler } from './common';
 
 export const app = express();
 
 app.use(json());
+app.use(loggingHandler);
 
 app.use('/bot', botRouter);
--- src/common/middlewares.ts
+++ src/common/middlewares.ts
@@ -23,3 +23,21 @@ export function rawTextMiddleware(
     next(err);
   });
 }
+
+export const loggingHandler = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  logging.log(
+    `Incomming - METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`,
+  );
+
+  res.on('finish', () => {
+    logging.log(
+      `Result - METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}] - STATUS: [${res.statusCode}]`,
+    );
+  });
+
+  next();
+};
